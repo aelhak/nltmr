b09b05810974ba96aaaab9e756098bd5447b642c
Ahmed Elhakeem <43848846+aelhak@users.noreply.github.com>
2021-09-16 08:47:24 +0100

    Update natural_cubic_spline_lme_models.R

--- natural_cubic_spline_lme_models.R
+++ natural_cubic_spline_lme_models.R
@@ -2,18 +2,18 @@
 # Using linear and natural cubic splines, SITAR, and latent trajectory models to characterise 
 # nonlinear longitudinal growth trajectories in cohort studies
 #
-# Elhakeem et al
+# Elhakeem et al: https://www.medrxiv.org/content/10.1101/2021.05.26.21257519v1
 #
 # NATURAL CUBIC SPLINE LME MODELS
 #
 
 library(lme4)
-library(spluti)
+library(spluti) # https://github.com/ZheyuanLi/spluti
 library(splines)
 library(tidyverse)
 library(patchwork)
 
-# ALSPAC FEMALES: DONE
+# ALSPAC FEMALES ####
 
 nslme_alsp_f <- map(3:7, possibly(~ { eval(parse(text = paste0(
   "lmer(tblh_bmc_ ~ ns(age, df = ", .x, ") + ( ns(age, df = 2) | id ), 
@@ -28,7 +28,7 @@ nslme_alsp_f_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %>
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_alsp_f_resid.csv") 
 
-(alsp_ns_f_pred <- data.frame(age = seq(min(alsp_f$age), max(alsp_f$age), length = 50)))
+(alsp_ns_f_pred <- data.frame(age = seq(min(alsp_f$age), max(alsp_f$age), length = 500)))
 (alsp_ns_f_pred$tblh_bmc_ <- predict(nslme_alsp_f_best, alsp_ns_f_pred, re.form = NA))
 (alsp_ns_f_mm <- model.matrix(terms(nslme_alsp_f_best), alsp_ns_f_pred))
 (alsp_ns_f_pred$se <- sqrt(diag(alsp_ns_f_mm %*% vcov(nslme_alsp_f_best) %*% t(alsp_ns_f_mm))))
@@ -59,7 +59,7 @@ nslme_alsp_m_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %>
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_alsp_m_resid.csv")  
 
-(alsp_ns_m_pred <- data.frame(age = seq(min(alsp_m$age), max(alsp_m$age), length = 50)))
+(alsp_ns_m_pred <- data.frame(age = seq(min(alsp_m$age), max(alsp_m$age), length = 500)))
 (alsp_ns_m_pred$tblh_bmc_ <- predict(nslme_alsp_m_best, alsp_ns_m_pred, re.form = NA))
 (alsp_ns_m_mm <- model.matrix(terms(nslme_alsp_m_best), alsp_ns_m_pred))
 (alsp_ns_m_pred$se <- sqrt(diag(alsp_ns_m_mm %*% vcov(nslme_alsp_m_best) %*% t(alsp_ns_m_mm))))
@@ -90,7 +90,7 @@ nslme_bmdcs_f_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_bmdcs_f_resid.csv")   
 
-(bmdcs_ns_f_pred <- data.frame(age = seq(min(bmdcs_f$age), max(bmdcs_f$age), length = 50)))
+(bmdcs_ns_f_pred <- data.frame(age = seq(min(bmdcs_f$age), max(bmdcs_f$age), length = 500)))
 (bmdcs_ns_f_pred$tblh_bmc_ <- predict(nslme_bmdcs_f_best, bmdcs_ns_f_pred, re.form = NA))
 (bmdcs_ns_f_mm <- model.matrix(terms(nslme_bmdcs_f_best), bmdcs_ns_f_pred))
 (bmdcs_ns_f_pred$se <- sqrt(diag(bmdcs_ns_f_mm %*% vcov(nslme_bmdcs_f_best) %*% t(bmdcs_ns_f_mm))))
@@ -121,7 +121,7 @@ nslme_bmdcs_m_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_bmdcs_m_resid.csv")  
 
-(bmdcs_ns_m_pred <- data.frame(age = seq(min(bmdcs_m$age), max(bmdcs_m$age), length = 50)))
+(bmdcs_ns_m_pred <- data.frame(age = seq(min(bmdcs_m$age), max(bmdcs_m$age), length = 500)))
 (bmdcs_ns_m_pred$tblh_bmc_ <- predict(nslme_bmdcs_m_best, bmdcs_ns_m_pred, re.form = NA))
 (bmdcs_ns_m_mm <- model.matrix(terms(nslme_bmdcs_m_best), bmdcs_ns_m_pred))
 (bmdcs_ns_m_pred$se <- sqrt(diag(bmdcs_ns_m_mm %*% vcov(nslme_bmdcs_m_best) %*% t(bmdcs_ns_m_mm))))
@@ -152,7 +152,7 @@ nslme_pbmas_f_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_pbmas_f_resid.csv")   
 
-(pbmas_ns_f_pred <- data.frame(age = seq(min(pbmas_f$age), max(pbmas_f$age), length = 50)))
+(pbmas_ns_f_pred <- data.frame(age = seq(min(pbmas_f$age), max(pbmas_f$age), length = 500)))
 (pbmas_ns_f_pred$tblh_bmc_ <- predict(nslme_pbmas_f_best, pbmas_ns_f_pred, re.form = NA))
 (pbmas_ns_f_mm <- model.matrix(terms(nslme_pbmas_f_best), pbmas_ns_f_pred))
 (pbmas_ns_f_pred$se <- sqrt(diag(pbmas_ns_f_mm %*% vcov(nslme_pbmas_f_best) %*% t(pbmas_ns_f_mm))))
@@ -183,7 +183,7 @@ nslme_pbmas_m_resid %>% group_by(visit) %>% select(tblh_bmc_, .fitted, .resid) %
   summarise_all(list(mean = ~round(mean(.,na.rm=T), 1), sd = ~round(sd(.,na.rm=T), 1))) %>% 
   write_csv("results/nslme_pbmas_m_resid.csv")   
 
-(pbmas_ns_m_pred <- data.frame(age = seq(min(pbmas_m$age), max(pbmas_m$age), length = 50)))
+(pbmas_ns_m_pred <- data.frame(age = seq(min(pbmas_m$age), max(pbmas_m$age), length = 500)))
 (pbmas_ns_m_pred$tblh_bmc_ <- predict(nslme_pbmas_m_best, pbmas_ns_m_pred, re.form = NA))
 (pbmas_ns_m_mm <- model.matrix(terms(nslme_pbmas_m_best), pbmas_ns_m_pred))
 (pbmas_ns_m_pred$se <- sqrt(diag(pbmas_ns_m_mm %*% vcov(nslme_pbmas_m_best) %*% t(pbmas_ns_m_mm))))
@@ -203,8 +203,8 @@ rm(nslme_pbmas_m, nslme_pbmas_m_bic, pbmas_ns_m_mm, pbmas_ns_m_spl_mean, nslme_p
 
 nspline_plot_fun <- function(data1, data2){ ggplot(
   data = data1, aes(x = age, y = tblh_bmc_, ymin = tblh_bmc_-(1.96*se), ymax = tblh_bmc_+(1.96*se))) +
-    geom_line(aes(col = "Males"), data = data1) + geom_line(aes(col = "Females"), data = data2) + theme_bw() +
-    geom_ribbon(data = data1, alpha=0.1) + geom_ribbon(data = data2, alpha=0.1) +
+    geom_line(aes(col = "Males"), data = data1) + geom_line(aes(col = "Females"), data = data2) +
+    geom_ribbon(data = data1, alpha=0.1) + geom_ribbon(data = data2, alpha=0.1) + theme_classic() +
     scale_colour_manual(name="legend", values=c("red2", "black")) + 
     scale_x_continuous(breaks=c(4, 8, 12, 16, 20, 24, 28, 32, 36, 40), limits=c(4, 40)) + 
     theme(legend.title = element_blank(), legend.position = c(0.15, 0.82),
@@ -227,7 +227,7 @@ nspline_vel_plot_fun <- function(data1, data2, apv1, apv2){ ggplot(
   data = data1, aes(x = x, y = y)) +
     geom_line(aes(col = "Males"), data = data1) + geom_line(aes(col = "Females"), data = data2) + 
     geom_vline(xintercept = apv1, linetype="dotted", color = "black") +
-    geom_vline(xintercept = apv2, linetype="dotted", color = "red2") + theme_bw() +
+    geom_vline(xintercept = apv2, linetype="dotted", color = "red2") + theme_classic() +
     scale_colour_manual(name="legend", values=c("red2", "black")) + 
     scale_x_continuous(breaks=c(4, 8, 12, 16, 20, 24, 28, 32, 36, 40), limits=c(4, 40)) + 
     theme(legend.title = element_blank(), legend.position = c(0.82, 0.82),
@@ -254,11 +254,11 @@ nspline_vel_pbmas <- nspline_vel_plot_fun(
 
 graphics.off()
 pdf("results/Fig4_nsplines.pdf",
-    width=9, height=10)
+    width = 8.6, height = 9.5)
 
 ((nspline_curve_alsp / nspline_curve_bmdcs / nspline_curve_pbmas) |
   (nspline_vel_alsp / nspline_vel_bmdcs / nspline_vel_pbmas)) + plot_layout(
-    widths = c(1.1, 1)) 
+    widths = c(1.2, 1)) 
 
 dev.off()
 
